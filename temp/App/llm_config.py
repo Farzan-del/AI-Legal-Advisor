8be380fa67545247a2e05b7308513973809ce41c
Sheikh Farzan Almaghazi <137151149+Farzan-del@users.noreply.github.com>
2025-08-04 15:38:23 +0500

    Delete temp directory

--- temp/App/llm_config.py
+++
@@ -1,8 +0,0 @@
-# llm_config.py
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-def get_gemini_llm():
-    return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
-        temperature=0.3
-    )
